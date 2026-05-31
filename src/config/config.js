db2cb41a77b00982f203e420f1d91db93ec8cd4f
tarun kumar <tarunkumar575022@gmail.com>
2026-05-31 14:33:01 +0530

    Force SSL on development environment for Render compatibility

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,13 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
     username: process.env.DB_USER,
